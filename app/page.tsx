e985224ce7b24194bfca942d0efc91d590963ef1
Matthias Dive <matthiasdive2@gmail.com>
2023-10-19 14:59:39 +0200

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
       <div className={'text-[#607B96] flex flex-col h-1/2'}>
-        <p>Hi all. I'm Matthias</p>
+        <p>Hi all. I&apos;m Matthias</p>
       </div>
   )
 }
